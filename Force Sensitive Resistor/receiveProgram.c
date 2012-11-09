01eca02066231922e59b9ba70f06dd851e465d9e
Stephen Shinn <shinnsm@rose-hulman.edu>
2012-11-09 14:04:19 -0500

    bug fixes

--- Force Sensitive Resistor/receiveProgram.c
+++ Force Sensitive Resistor/receiveProgram.c
@@ -6,40 +6,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "XBee.h"
+
+#define SYSFS_GPIO_DIR "/sys/class/gpio"
 
 //reads the next int sent to uart 2
 int receiveIntXbee()
 {	
 	int value;
-	FILE* file = fopen("/dev/ttyO2", "r");
-	fscanf(file, "%d", &value);
-	fclose(file);
-	printf("read value: %d\n", value);
-    	return 0;
-}
-
-//write value to uart 2
-int sendIntXbee(int value)
-{
-	FILE* file = fopen("/dev/ttyO2", "w+");
-	fprintf(file, "%d", &value);
-	fclose(file);
-    	return 0;
-
-}
-
-int sendShortXbee(short value)
-{
-	FILE* file = fopen("/dev/ttyO2", "w+");
-	fprintf(file, "%d", &value);
-	fclose(file);
-    	return 0;
-}
-
-int receiveShortXbee()
-{	
-	short value;
 	FILE* file = fopen("/dev/ttyO2", "r");
 	fscanf(file, "%d", &value);
 	fclose(file);
@@ -98,10 +71,9 @@ void main(int argc, char **argv, char **envp) {
 	
 	// Keep going
 	while (1) {
-	int analogValue = receiveIntXbee();
+		int analogValue = receiveIntXbee();
 
-	// Do anything based on analog value
-	if (analogValue < 1000)
-		lightLedIfStrong();
+		// Do anything based on analog value
+		lightLedIfStrong(analogValue);
 	}
 }
--- Force Sensitive Resistor/sendProgram.c
+++ Force Sensitive Resistor/sendProgram.c
@@ -6,9 +6,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "XBee.h"
-
-#define SYSFS_GPIO_DIR "/sys/class/gpio"
 
 // Read analog value and print to console
 int readAnalog() {
@@ -21,23 +18,29 @@ int readAnalog() {
 	return num;
 }
 
+int sendIntXbee(int value)
+{
+	FILE* file = fopen("/dev/ttyO2", "w+");
+	fprintf(file, "%d \n", value);
+	fclose(file);
+	return 0;
+}
+
+int initializeXbee() {
+	//set pin 21 to uart 2 TX - output
+	FILE* file = fopen("/sys/kernel/debug/omap_mux/spi0_d0", "w+");
+	fprintf(file, "1");
+	fclose(file);
+
+	//set pin 22 to uart 2 RX - input
+	file = fopen("/sys/kernel/debug/omap_mux/spi0_sclk", "w+");
+	fprintf(file, "21");
+	fclose(file);
+	return 0;
+}
+
 // Main
 void main(int argc, char **argv, char **envp) {
-	// Export gpio60
-	FILE *fp;
-	char set_value[5];
-	fp = fopen(SYSFS_GPIO_DIR "/export", "ab");
-	rewind(fp);
-	strcpy(set_value, "60");
-	fwrite(&set_value, sizeof(char), 2, fp);
-	fclose(fp);
-
-	// Set gpio60 direction
-	FILE *file;
-	file = fopen("/sys/class/gpio/gpio60/direction","w+");
-	fprintf(file,"%s","out");
-	fclose(file);
-	
 	// initialize XBee
 	initializeXbee();
 	
@@ -46,7 +49,7 @@ void main(int argc, char **argv, char **envp) {
 		int analogValue = readAnalog();
 		sendIntXbee(analogValue);
 		
-		sleep(0.5);
+		sleep(0.1);
 	}
 }
 
