0132e4beafed3cb2fd39ca9e7c9eca00bf3183ac
Matthew Moravec <mmoravec09@gmail.com>
2012-11-12 13:20:06 -0800

    added xbee.c files to my folder

---
+++ 2AxisJoystick/XBee.h
@@ -0,0 +1,9 @@
+// Header file for XBee helper functions
+// Josh Dugan, Stephen Shinn, Matt Moravec
+
+// Function prototypes
+int receiveIntXbee();
+int sendIntXbee(int value);
+int sendShortXbee(short value);
+short receiveShortXbee();
+int initializeXBEE();
